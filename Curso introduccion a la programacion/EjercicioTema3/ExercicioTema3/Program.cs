7a31e308f691be52b1ce697a4d556b3e7d58a9f6
zomeze93 <zomeze93@icloud.com>
2022-11-03 20:15:08 +0100

    Ejercicio tema 3

---
+++ Curso introduccion a la programacion/EjercicioTema3/ExercicioTema3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace EjecicioTema3
+{
+    class Ejecicio
+    {
+        //Parte uno
+        static void Main(string[] args)
+        {
+            int primer = 5;
+            int segundo = 2;
+            int tercero = 9;
+
+            Suma(primer, segundo, tercero);
+
+            //Suma ejercicio parte dos
+            coche miCoche = new coche();
+            miCoche.SumaPuertas();
+            Console.WriteLine(miCoche.puertas);
+            Console.Read();
+            //
+        }
+
+        private static int Suma(int a, int b, int c)
+        {
+            return (a + b + c);
+        }
+
+        //
+        //Parte dos
+        class coche
+        {
+            public int puertas = 0;
+
+            public void SumaPuertas()
+            {
+                puertas += 1;
+            }
+        }
+    }
+
+    
+}
+
